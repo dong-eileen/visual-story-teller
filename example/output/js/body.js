e8d43f528229720e2fc22bd8d9825d3872c46740
dong-eileen <dong.eileen2@gmail.com>
2019-09-30 20:59:45 -0500

    fix the broken portrait overlay

--- example/output/js/body.js
+++ example/output/js/body.js
@@ -1,21 +1,7 @@
-
-// $(document).ready(function() {
-
-//     $('a.name-tag').attr('target', '_blank');
-
-//     $('.name-tag').popover({ 
-//         html: true,
-//         trigger: 'hover',
-//         placement: 'bottom',
-//         content: function () {
-//             return '<img src="'+ $(this).attr('href') + '" width=400px/>';
-//         }
-//     });
-// });
-
 const displayBox = document.getElementById("display-box");
 
 const getCharacterDetails = id => getDetailsFor(getNameFromId(id));
+const getNameFromId = id => id.split('_')[0];
 
 const initializeDisplayBox = () => {
 	const characters = document.getElementsByClassName("name-tag");
@@ -29,7 +15,13 @@ const initializeDisplayBox = () => {
 	}
 }
 
-const getNameFromId = id => id.split('_')[0];
+const initializeNameTags = () => {
+	const characters = document.getElementsByClassName("name-tag");
+	for (const character of characters) {
+		character.target = "_blank";
+		character.href = getCharacterDetails(character.id).imagePath;
+	}
+}
 
 const customizeDisplayBox = id => {
 	const character = getCharacterDetails(id);
@@ -44,14 +36,6 @@ const showDisplayBox = event => {
 	displayBox.style.top = event.pageY + 20;
 	displayBox.style.left = event.pageX + 20;
 	displayBox.style.display = "block";
-}
-
-const initializeNameTags = () => {
-	const characters = document.getElementsByClassName("name-tag");
-	for (const character of characters) {
-		character.target = "_blank";
-		character.href = getCharacterDetails(character.id).imagePath;
-	}
 }
 
 initializeDisplayBox();
