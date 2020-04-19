27ed80b496e49d18fb97648fe5a01bf8abddc5bf
dong-eileen <dong.eileen2@gmail.com>
2020-04-19 11:48:42 -0500

    update output

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # visual-story-teller
 
 A text converter for turning play/script-structured text into HTML5 pages with images.
+
+## Usage
+Need to fix (put input in parent directory of visual-story-teller and run from within /visual-story-teller.)
--- example/output/js/characters.js
+++ example/output/js/characters.js
@@ -1,12 +1,13 @@
 let characters = {
-    alice: {
-        name: "Alice",
-        imagePath: "./images/alice.png"
-    },
-    bob: {
-        name: "Bob",
-        imagePath: "./images/bob.png"
-    }
-}
+
+        alice: {
+            name: "Alice",
+            imagePath: "./images/alice.png"
+        },
+        bob: {
+            name: "Bob",
+            imagePath: "./images/bob.png"
+        },
+};
 
 const getDetailsFor = name => characters[name];
